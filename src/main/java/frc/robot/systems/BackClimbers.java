6c5636b4338b5e32b344bf4155f7b0362c749936
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2019-02-02 21:47:45 +0200

    BackClimbers created from existing FrontClimbers class

---
+++ src/main/java/frc/robot/systems/BackClimbers.java
@@ -0,0 +1,100 @@
+package frc.robot.systems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import frc.robot.components.LimitSwitch;
+import frc.robot.components.speed.SwitchRangedController;
+
+/**
+ * The climbers in the back of the robot used to fix the angle for climbing lvl 2/3
+ * Utilizes two front "spider legs" which extend below the robot with a wheel at the end
+ * Controls the legs through a limitswitch-ranged motor on each side
+ * 
+ * Note: (RAW) Positive power on left motor should represent extending
+ * Note: (RAW) Positive power on right motor should represent retracting
+ * 
+ */
+public class BackClimbers{
+
+    /**
+     * The power granted during extension
+     */
+    public double EXTEND_POWER = 1.0;
+
+    /**
+     * The power granted during retraction
+     */
+    public double RETRACT_POWER = -1.0;
+
+    /**
+     * The switch ranged controller representing left
+     */
+    private SwitchRangedController leftMotor;
+    /**
+     * The switch ranged controller representing right
+     */
+    private SwitchRangedController rightMotor;
+
+    /**
+     * Initializes the back climbers with the following
+     * @param leftMotor - motor on the left side of the robot
+     * @param leftRetractedSwitch - switch that is activated when left is fully retracted
+     * @param leftExtendedSwitch - switch that is activated when left is fully extended
+     * @param rightMotor - motor on the right side of the robot
+     * @param rightRetractedSwitch - switch that is activated when right is fully retracted
+     * @param rightExtendedSwitch - switch that is activated when right is fully extended
+     */
+    public BackClimbers (SpeedController leftMotor, LimitSwitch leftRetractedSwitch, LimitSwitch leftExtendedSwitch,
+    SpeedController rightMotor, LimitSwitch rightRetractedSwitch, LimitSwitch rightExtendedSwitch){
+
+        // Don't invert the left motor
+        leftMotor.setInverted(false);
+        
+        // Initialize the left motor ranged controller with
+        // the left extended switch representing the switch that halts motion in the + [Raw] (extending) direction when activated
+        // and the left retracted switch representing the switch that halts motion in the - [Raw] (retracting) direction when activated
+        leftMotor = new SwitchRangedController(leftMotor, leftExtendedSwitch, leftRetractedSwitch);
+
+        // Invert the right motor (due to symmetry)
+        rightMotor.setInverted(true);
+
+        // Initialize the right motor ranged controller with
+        // the right extended switch representing the switch that halts motion in the + [Set] (extending) direction when activated
+        // and the right retracted switch representing the switch that halts motion in the - [Set] (retracting) direction when activated
+        rightMotor = new SwitchRangedController(rightMotor, rightExtendedSwitch, rightRetractedSwitch);
+
+    }
+
+    /**
+     * Extends both front motors together (each will independently halt when they hit their switch)
+     * @return - true if both finished
+     */
+    public boolean extend (){
+        return this.leftMotor.setControlled(EXTEND_POWER) && this.rightMotor.setControlled(EXTEND_POWER);
+    }
+
+    /**
+     * Retracts both front motors together (each will independently halt when they hit their switch)
+     * @return - true if both finished
+     */
+    public boolean retract (){
+        return this.leftMotor.setControlled(RETRACT_POWER) && this.rightMotor.setControlled(RETRACT_POWER);
+    }
+
+    /**
+     * Whether or not both legs are fully extended
+     * @return - true if both fully extended
+     */
+    public boolean isFullyExtended(){
+        return this.leftMotor.fowardSwitchActivated() && this.rightMotor.fowardSwitchActivated();
+    }
+
+    /**
+     * Whether or not both legs are fully retracted
+     * @return - true if both fully retracted
+     */
+    public boolean isFullyRetracted(){
+        return this.leftMotor.reverseSwitchActivated() && this.rightMotor.reverseSwitchActivated();
+    }
+
+
+}
