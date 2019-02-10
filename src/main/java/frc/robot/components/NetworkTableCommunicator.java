01d81a3d6a756a23876a6d483b7503933a1f3a40
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2019-02-10 11:19:37 +0200

    Added Motor, Solenoid, and IO network tables

--- src/main/java/frc/robot/components/NetworkTableCommunicator.java
+++ src/main/java/frc/robot/components/NetworkTableCommunicator.java
@@ -12,11 +12,6 @@ import frc.robot.constants.network.SmartDashboardConstants;
 public class NetworkTableCommunicator{
 
     /**
-     * The name for the smart dashboard table
-     */
-    public static final String SMART_DASHBOARD_NAME = "SmartDashboard";
-
-    /**
      * The global network table instance
      */
     private static NetworkTableInstance tableInstance;
@@ -29,7 +24,20 @@ public class NetworkTableCommunicator{
         tableInstance = NetworkTableInstance.getDefault();
         // Init the smart dashboard
         initSmartDashboard();
+        // Init the motor dashboard
+        initMotorDashboard();
+        // Init the solenoid dashboard
+        initSolenoidDashboard();
+        // Init the IO dashboard
+        initIoDashboard();
     }
+
+    /** ---- Smart Dashboard ---- */
+
+    /**
+     * The name for the smart dashboard table
+     */
+    public static final String SMART_DASHBOARD_NAME = "SmartDashboard";
 
     /**
      * The smart dashboard network table
@@ -66,6 +74,123 @@ public class NetworkTableCommunicator{
      */
     public static NetworkTableValue get (SmartDashboardConstants loc){
         return smartDashboard.getEntry(loc.getKey()).getValue();
+    }
+
+    /** ---- Motor Dashboard ---- */
+
+    /**
+     * The name for the motor dashboard table
+     */
+    public static final String MOTOR_DASHBOARD_NAME = "MotorDashboard";
+
+    /**
+     * The motor dashboard network table
+     */
+    private static NetworkTable motorDashboard;
+
+    /**
+     * Initializes the motor dashboard
+     */
+    private static void initMotorDashboard (){
+        // Retrieve the motor dashboard table
+        motorDashboard = tableInstance.getTable(SMART_DASHBOARD_NAME);
+    }
+
+    /**
+     * Sets the value of the entry given by loc to value
+     * @param loc - the referenced entry
+     * @param value - the value to set it to
+     */
+    public static void setMotorDashboardValue (String loc, NetworkTableValue value){
+        // Get the entry at loc and set the value
+        motorDashboard.getEntry(loc).setValue(value);
+    }
+
+    /**
+     * Retrieves the value for the loc at the table
+     * @param loc - the referenced entry
+     * @return - the current value at that entry
+     */
+    public static NetworkTableValue getMotorDashboardValue (String loc){
+        return motorDashboard.getEntry(loc).getValue();
+    }
+
+    /** ---- Solenoid Dashboard ---- */
+
+    /**
+     * The name for the solenoid dashboard table
+     */
+    public static final String SOLENOID_DASHBOARD_NAME = "SolenoidDashboard";
+
+    /**
+     * The solenoid dashboard network table
+     */
+    private static NetworkTable solenoidDashboard;
+
+    /**
+     * Initializes the solenoid dashboard
+     */
+    private static void initSolenoidDashboard (){
+        // Retrieve the solenoid dashboard table
+        solenoidDashboard = tableInstance.getTable(SOLENOID_DASHBOARD_NAME);
+    }
+
+    /**
+     * Sets the value of the entry given by loc to value
+     * @param loc - the referenced entry
+     * @param value - the value to set it to
+     */
+    public static void setSolenoidDashboardValue (String loc, NetworkTableValue value){
+        // Get the entry at loc and set the value
+        solenoidDashboard.getEntry(loc).setValue(value);
+    }
+
+    /**
+     * Retrieves the value for the loc at the table
+     * @param loc - the referenced entry
+     * @return - the current value at that entry
+     */
+    public static NetworkTableValue getSolenoidDashboardValue (String loc){
+        return solenoidDashboard.getEntry(loc).getValue();
+    }
+    
+    /** ---- I/O Dashboard ---- */
+
+    /**
+     * The name for the solenoid dashboard table
+     */
+    public static final String IO_DASHBOARD_NAME = "IODashboard";
+
+    /**
+     * The io dashboard network table
+     */
+    private static NetworkTable ioDashboard;
+
+    /**
+     * Initializes the io dashboard
+     */
+    private static void initIoDashboard (){
+        // Retrieve the io dashboard table
+        ioDashboard = tableInstance.getTable(IO_DASHBOARD_NAME);
+    }
+
+    /**
+     * Sets the value of the entry given by loc to value
+     * @param loc - the referenced entry
+     * @param value - the value to set it to
+     */
+    public static void setIoDashboardValue (String loc, NetworkTableValue value){
+        // Get the entry at loc and set the value
+        ioDashboard.getEntry(loc).setValue(value);
+    }
+
+    /**
+     * Retrieves the value for the loc at the table
+     * @param loc - the referenced entry
+     * @return - the current value at that entry
+     */
+    public static NetworkTableValue getIoDashboardValue (String loc){
+        return ioDashboard.getEntry(loc).getValue();
     }
 
 
